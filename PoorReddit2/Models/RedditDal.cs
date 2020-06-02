6cbf21a3e3e435d2b943f82d539fc951a4d93b2c
Josh Forner <fornerjos@gmail.com>
2020-06-01 23:27:38 -0400

    first commit

---
+++ PoorReddit2/Models/RedditDal.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PoorReddit2.Models
+{
+    public class RedditDal
+    {
+        public string GetAPIString(string subreddit)
+        {
+            string url = $"https://www.reddit.com/r/{subreddit}/.json";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string output = rd.ReadToEnd();
+            return output;
+        }
+
+        public RedditPost GetPost(string sr)
+        {
+            string output = GetAPIString(sr);
+            RedditPost rp = JsonConvert.DeserializeObject<RedditPost>(output); 
+            return rp;
+        }
+    }
+}
